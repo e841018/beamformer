032fc1e7c78acab5ba98ab71beccf2df0df7dc56
Ashu <e841018@gmail.com>
2021-05-09 00:00:58 +0800

    add directives.tcl

---
+++ HLS/reports/solution2_pipeline_L2/directives.tcl
@@ -0,0 +1,20 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
+############################################################
+set_directive_interface -mode s_axilite -bundle control "beamformer"
+set_directive_interface "beamformer"
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemi "beamformer" RXi_in1
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemi "beamformer" Bi_out
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemq "beamformer" RXq_in1
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemi "beamformer" Wi_in2
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemq "beamformer" Wq_in2
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemq "beamformer" Bq_out
+set_directive_interface -mode s_axilite -bundle control "beamformer" RXi_in1
+set_directive_interface -mode s_axilite -bundle control "beamformer" RXq_in1
+set_directive_interface -mode s_axilite -bundle control "beamformer" Wi_in2
+set_directive_interface -mode s_axilite -bundle control "beamformer" Wq_in2
+set_directive_interface -mode s_axilite -bundle control "beamformer" Bi_out
+set_directive_interface -mode s_axilite -bundle control "beamformer" Bq_out
+set_directive_pipeline -II 1 "beamformer/loop2"
---
+++ HLS/reports/solution4_partition/directives.tcl
@@ -0,0 +1,27 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
+############################################################
+set_directive_interface -mode s_axilite -bundle control "beamformer"
+set_directive_interface "beamformer"
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemi "beamformer" RXi_in1
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemi "beamformer" Bi_out
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemq "beamformer" RXq_in1
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemi "beamformer" Wi_in2
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemq "beamformer" Wq_in2
+set_directive_interface -mode m_axi -depth 128 -offset slave -bundle gmemq "beamformer" Bq_out
+set_directive_interface -mode s_axilite -bundle control "beamformer" RXi_in1
+set_directive_interface -mode s_axilite -bundle control "beamformer" RXq_in1
+set_directive_interface -mode s_axilite -bundle control "beamformer" Wi_in2
+set_directive_interface -mode s_axilite -bundle control "beamformer" Wq_in2
+set_directive_interface -mode s_axilite -bundle control "beamformer" Bi_out
+set_directive_interface -mode s_axilite -bundle control "beamformer" Bq_out
+set_directive_pipeline -II 1 "beamformer/loop2"
+set_directive_pipeline -II 1 "beamformer/readA"
+set_directive_pipeline -II 1 "beamformer/readB"
+set_directive_pipeline -II 1 "beamformer/writeC"
+set_directive_array_partition -type complete -dim 2 "beamformer" RXi
+set_directive_array_partition -type complete -dim 2 "beamformer" RXq
+set_directive_array_partition -type complete -dim 2 "beamformer" Wi
+set_directive_array_partition -type complete -dim 2 "beamformer" Wq
